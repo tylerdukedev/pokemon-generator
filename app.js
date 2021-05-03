8e9e0f80d3691f6b40432f591e314b80463e5723
freshnewkicks <fenixg1@gmail.com>
2021-05-02 20:07:55 -0400

    js

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-
-
 let con = document.querySelector('#container');
-function generate(){
-    let ask = document.querySelector('#input');
-    if (ask.value > 0 && ask.value < 152){
-        con.innerHTML = '';
-        for (i = 0; i < ask.value; i++) {
-            let pokeball = document.createElement('img');
-            pokeball.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Math.floor(
-                Math.random() * 152
-            )}.png`;
-            con.append(pokeball);
-            console.log(`${ask.value}`);
-        }
-    } else if (ask.value > 152 || ask.value < 0){
-        con.innerHTML = '';
-        con.appendChild('no');
-    }
+function generate() {
+	let ask = document.querySelector('#input');
+	if (ask.value > 0 && ask.value < 152) {
+		con.innerHTML = '';
+		for (i = 0; i < ask.value; i++) {
+			let pokeball = document.createElement('img');
+			pokeball.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Math.floor(
+				Math.random() * 152
+			)}.png`;
+			con.append(pokeball);
+			console.log(`${ask.value}`);
+		}
+	} else if (ask.value > 152 || ask.value < 0) {
+		con.innerHTML = '';
+		con.appendChild('no');
+	}
 }
-
-
 
 // for (i = 0; i < 7; i++){
 //     let pokeball = document.createElement('img');
